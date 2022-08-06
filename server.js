334143213fa9695bc47b7be50c907464284ad40f
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2022-08-05 23:09:52 -0400

    manga seed array created

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3003;
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo')
+});
 
 //setting up views
 app.set('view engine', "jsx");
